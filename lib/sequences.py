07d9ae1111f07e36a8475a2baa664bd963643e3c
Tedd James <tedd.james@student.moringaschool.com>
2025-05-07 11:43:34 +0300

    Sequences (CodeGrade)

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        seq = []
+    elif n == 1:
+        seq = [0]
+    else:
+        seq = [0, 1]
+        for _ in range(2, n):
+            seq.append(seq[-1] + seq[-2])
+
+    print(seq)
+    return seq
+
+
+if __name__ == "__main__":
+    print_fibonacci(5)  
